51f1b2a0df49c977d24109ceed31c55d601ef3a8
John <jmitropoulos@alumni.berklee.edu>
2022-02-08 22:50:00 -0600

    Add new action intents n pass info from home activity to main

---
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -0,0 +1,57 @@
+package com.example.firstapp
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock
+import android.widget.EditText
+import android.widget.TextView
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var homeTv:TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        homeTv = findViewById(R.id.tvHome)
+        //       String name = getIntent().getExtras().getString("namekey");
+        var name = intent.extras?.getString("namekey")
+        homeTv.text = name
+    }
+
+    fun handleClick(view: android.view.View) {
+        when(view.id){
+            R.id.btnDial -> {startDialer()}
+            R.id.btnTimer -> {startTimer("milk", 10)}
+        }
+
+    }
+
+    fun startTimer(message: String, seconds: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_LENGTH, seconds)
+            putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    private fun startDialer() {
+        var dIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:123456")) //implicit intent
+
+        startActivity(dIntent)
+    }
+
+    fun closeActivity(view: android.view.View) { //step 2
+        //send the contact back to main activity
+        var etContact = findViewById<EditText>(R.id.etContact)
+        var phno = etContact.text.toString()
+        var dataIntent : Intent = Intent()
+        dataIntent.putExtra("phonenum", phno)
+        setResult(RESULT_OK,dataIntent)
+        finish()
+    }
+}
--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.firstapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnFocusChangeListener {
+    EditText nameEditText; //declaration
+    public static  String TAG = MainActivity.class.getSimpleName();
+    //"MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //inflation
+        nameEditText = findViewById(R.id.etName); //initialized it -- getting handle
+
+        nameEditText.setOnFocusChangeListener(this); //this referes to the current instance of this class
+
+        TextView mTextView = new TextView(this);
+        mTextView.setText("text from java");
+
+        ConstraintLayout cl = findViewById(R.id.container);
+        cl.addView(mTextView);
+    }
+
+    public void clickHandler(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                startHome();
+                break;
+            case R.id.btnAlarm:
+                createAlarm("midnight",00,49);
+                break;
+        }
+        // startHome();
+
+    }
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void startHome() {
+        String value = nameEditText.getText().toString();
+        Intent hIntent = new Intent(this,HomeActivity.class); //explicit intent
+        hIntent.putExtra("namekey",value);
+        startActivityForResult(hIntent,123); //step 1
+    }
+    //because in whatsapp chat.. you can go to fetch a contact/photo, location
+    //the point of return is same ie onActivityResult how do i differentiate which data you're getting
+    //that differentiation is made using the request
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) { //dataIntent
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 123){
+            String phno =  data.getExtras().getString("phonenum");
+            TextView tvMain = findViewById(R.id.tvMain);
+            tvMain.setText(phno);
+        }
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+        if(hasFocus){
+            Toast.makeText(this, "got focus", Toast.LENGTH_SHORT).show();
+            Log.i(TAG,"has focus");
+        }
+        else {
+            Toast.makeText(this, R.string.enter_pw, Toast.LENGTH_SHORT).show();
+            Log.i(TAG,"lost focus");
+
+        }
     }
 }
